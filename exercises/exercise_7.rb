a5b6bc106a8cf4c3f049737c647274de89791cf1
roylyh <roy.louyihui@gmail.com>
2023-04-05 14:14:45 -0700

    debug code

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+invalid = Store.new(name: "sh", annual_revenue: 1234, mens_apparel: true, womens_apparel: true)
+p invalid, invalid.valid?, invalid.errors.messages
+
+invalid = Store.new(name: "shs", annual_revenue: 243, mens_apparel: false, womens_apparel: false)
+p invalid, invalid.valid?, invalid.errors.messages
+
+invalid = @store1.employees.new(first_name: "", last_name: "asdf", hourly_rate: 234)
+p invalid, invalid.valid?, invalid.errors.messages
+
+puts "Enter your store name"
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+puts store.errors.messages
